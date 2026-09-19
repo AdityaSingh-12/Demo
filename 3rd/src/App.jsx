d5bc634f3bb279687fb14d637823668318c88e16
AdityaSingh-12 <winterrendering@gmail.com>
2026-09-20 00:56:45 +0530

    fragments

---
+++ 3rd/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
+
+function App() {
+ return (<>
+    <h1 className="App">healthy food</h1>
+      
+      <ul class="list-group">
+  <li class="list-group-item">Dal</li>
+  <li class="list-group-item">chawal</li>
+  <li class="list-group-item">panner</li>
+  <li class="list-group-item">banana</li>
+  <li class="list-group-item">fruits</li>
+</ul>
+</>
+        );
+}
+export default App
